b369b610646195927a6d9807f92b50b637787a3f
Yuichiro Izumi <yizumi1012xxx@gmail.com>
2023-08-16 11:59:12 +0900

    fix ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "browser-bundler",
   plugins: [react()],
   test: {
     setupFiles: ["./src/test/setup.ts"],
